67bd1801abd6c67a10af628266ef148585900e2b
Georgy Bunin <georgyb@mprest.com>
2020-01-09 12:35:12 +0200

    print base64

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,30 @@
 import { Component } from '@angular/core';
 
+declare const html2canvas;
+
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div id="hello">
+      <p>
+        Start editing to see some magic happen :)
+      </p>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+
+    <button (click)="generate($event)">generage image</button>
+
+    <hr />
+    <pre>{{ base64 }}</pre>
+    <hr />
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'ngScrenshot';
+  base64;
+  generate($event) {
+    html2canvas(document.querySelector('#hello')).then(canvas => {
+      document.body.appendChild(canvas);
+      this.base64 = document.getElementsByTagName('canvas')[0].toDataURL();
+    });
+  }
 }
